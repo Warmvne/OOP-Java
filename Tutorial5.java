d6da96d0c0c0af42bdb3981ac54c5c6482d41de1
Warmvne <118521759+Warmvne@users.noreply.github.com>
2023-01-29 16:02:17 +0000

    Update Tutorial5.java

--- Tutorial5.java
+++ Tutorial5.java
@@ -139,3 +139,136 @@ class LabCourse extends CollegeCourse{
     }
 }
 ///////////////////////////////// Question 3 //////////////////////////////////////
+
+public class Main
+{
+	public static void main(String[] args) {
+	    PreferredCustomer FavCustomer= new PreferredCustomer();
+	    FavCustomer.setName("Marwane");
+	    FavCustomer.setAddress("10 Downing Street, London");
+	    FavCustomer.setPhoneNumber("unknown");
+	    FavCustomer.setCustomerNumber(95);
+	    FavCustomer.setMailList(false);
+	    FavCustomer.setAmountPurchase(1010);
+	    
+	    System.out.println("Customer's number: "+ FavCustomer.getCustomerNumber());
+	    System.out.println("Customer's name: "+ FavCustomer.getName());
+	    System.out.println("Customer's address: "+ FavCustomer.getAddress());
+	    System.out.println("Customer's phone number: "+ FavCustomer.getPhoneNumber());
+	    if(FavCustomer.IsMailList()) System.out.println(FavCustomer.getName()+" is subscribed to the newsletter.");
+	    else System.out.println(FavCustomer.getName()+" is not subscribed to the newsletter.");
+	    System.out.println("Amount purchase: "+FavCustomer.getAmountPurchase());
+	    System.out.println("Discount on your next purchase: "+ FavCustomer.getDiscount(FavCustomer.getAmountPurchase())*100+"%");
+	}
+}
+
+class Person{
+    private String name;
+    private String address;
+    private String phone_number;
+    
+    public Person(){
+        name="";
+        address="";
+        phone_number="";
+    }
+    public Person(String name,String address,String phone_number){
+        this.name=name;
+        this.address=address;
+        this.phone_number=phone_number;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String nom){
+        name=nom;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public void setAddress(String adresse){
+        address=adresse;
+    }
+    
+    public String getPhoneNumber(){
+        return phone_number;
+    }
+    
+    public void setPhoneNumber(String phone){
+        phone_number=phone;
+    }
+}
+
+class Customer extends Person{
+    private int CustomerNumber;
+    private boolean email_list;
+    
+    public Customer(){
+        super();
+        CustomerNumber=0;
+        email_list=false;
+    }
+    
+    public Customer(String name, String address, String phone_number, int CustomerNumber, boolean email_list){
+        super(name,address,phone_number);
+        this.CustomerNumber=CustomerNumber;
+        this.email_list=email_list;
+    }
+    
+    public int getCustomerNumber(){
+        return CustomerNumber;
+    }
+    
+    public void setCustomerNumber(int num){
+        CustomerNumber=num;
+    }
+    
+    public boolean IsMailList(){
+        return email_list;
+    }
+    
+    public void setMailList(boolean mail){
+        email_list=mail;
+    }
+}
+
+class PreferredCustomer extends Customer{
+    private double amount_purchase;
+    private double discount;
+    
+    public PreferredCustomer(){
+        super();
+        amount_purchase=0;
+        discount=0;
+    }
+    
+    public PreferredCustomer(String name, String address, String phone_number, int CustomerNumber, boolean email_list,double amount_purchase){
+        super(name,address,phone_number,CustomerNumber,email_list);
+        this.amount_purchase=amount_purchase;
+        discount=getDiscount(amount_purchase);
+    }
+    
+    public double getAmountPurchase(){
+        return amount_purchase;
+    }
+    
+    public void setAmountPurchase(double amount){
+        amount_purchase=amount;
+    }
+    
+    public double getDiscount(double amount_purchase){
+        if(amount_purchase>2000) return 0.1;
+        else if(amount_purchase>1500) return 0.07;
+        else if(amount_purchase>1000) return 0.06;
+        else if(amount_purchase>500) return 0.05;
+        else return 0;
+    }
+    
+    public double getDiscount(){
+        return discount;
+    }
+}
