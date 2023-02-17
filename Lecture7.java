b16ae1be06fb18516fa4a22b8883bf90f615e15b
Warmvne <118521759+Warmvne@users.noreply.github.com>
2023-02-17 13:50:26 +0000

    Update Lecture7.java

--- Lecture7.java
+++ Lecture7.java
@@ -34,3 +34,84 @@ public class TrainingExam {
     }
     
 }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/**************************************************************************************************************************************************************************************************************************************
+
+
+Write a class named TestScores. The class constructor should accept an array of test scores as its argument. 
+The class should have a method that returns the average of the test scores. 
+If any test score in the array is negative or greater than 100, the class should throw an IllegalArgumentException.
+Demonstrate the class in a program. 
+
+***************************************************************************************************************************************************************************************************************************************/
+
+
+package lecture7;
+import java.util.Scanner;
+/**
+ *
+ * @author 932254331
+ */
+public class Lecture7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[]array=new int[5];
+        TestScores test1 = new TestScores(array);
+        test1.FillArray();
+        System.out.println("Average of this test is "+ test1.AverageGrade());
+    }
+    
+}
+
+class TestScores{
+    private final int MAX_GRADE =100;
+    private final int MIN_GRADE=0;
+    private int[] array= new int[5];
+    
+   public TestScores(int[]array){
+       this.array=array;
+   }
+   
+   public int AverageGrade(){
+       int sum=0;
+       for(int i=0;i<array.length;i++){
+           sum+=array[i];
+       }
+       int avg=sum/array.length;
+       return avg;
+   }
+   
+   public void FillArray(){
+       int grade;
+       Scanner input = new Scanner(System.in);
+       for(int i=0;i<array.length;i++){
+           try{
+               System.out.println("Input a grade: ");
+               grade=input.nextInt();
+               if(grade<MIN_GRADE|| grade>MAX_GRADE){
+                    throw new IllegalArgumentException("The grade is not between 0 and 100");
+               }
+               else{
+                    array[i]=grade;
+               }
+           }
+           catch(IllegalArgumentException mistake){
+               System.out.println(mistake.getMessage());
+               System.out.println("The grade has been registered as zero");
+               grade=0;
+               array[i]=grade;
+               
+           }
+       }
+   }
+    
+}
